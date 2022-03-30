b624d75ea2c48b6c1358e5856d16ebe28738c134
sea-comet <2035340085@qq.com>
2022-03-31 00:47:45 +0800

    changed config file for colab

--- src/config.py
+++ src/config.py
@@ -10,6 +10,12 @@ BRATS_TRAIN_FOLDERS = "./../Dataset_small/BraTS2020_TrainingData/MICCAI_BraTS202
 BRATS_VAL_FOLDER = "./../Dataset_small/BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData"
 BRATS_TEST_FOLDER = "./../Dataset_small/BraTS2020_TestingData/MICCAI_BraTS2020_TestingData"
 
+'''
+用于colab的路径
+BRATS_TRAIN_FOLDERS = "./../drive/My Drive/Dataset_small/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData"
+BRATS_VAL_FOLDER = "./../drive/My Drive/Dataset_small/BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData"
+BRATS_TEST_FOLDER = "./../drive/My Drive/Dataset_small/BraTS2020_TestingData/MICCAI_BraTS2020_TestingData"
+'''
 
 def get_brats_folder(on="val"):
     if on == "train":
